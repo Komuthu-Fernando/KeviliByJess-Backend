7977edd277c962856055edeb25e495cc5b6c8366
Komuthu <komuthuapsara@gmail.com>
2025-04-01 11:47:14 +0530

    port change to render

--- server.js
+++ server.js
@@ -13,5 +13,8 @@ app.use(cors());
 
 app.use("/api", orderRoutes);
 
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${PORT}`));
